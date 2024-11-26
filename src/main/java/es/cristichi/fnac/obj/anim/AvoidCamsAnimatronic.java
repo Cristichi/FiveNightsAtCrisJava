918c7a8b581937eee04289db6a7d22dbc77700fb
Cristichi <cristichi@hotmail.es>
2024-11-26 17:23:43 +0100

    Small refractor to the name of AvoidCamsMovementAnimatronic.java.

--- src/main/java/es/cristichi/fnac/obj/anim/AvoidCamsAnimatronic.java
+++ src/main/java/es/cristichi/fnac/obj/anim/AvoidCamsAnimatronic.java
@@ -10,12 +10,12 @@ import java.util.*;
 /**
  * This class implements {@link Animatronic} with a default movement that simply avoids a list of Camera names.
  */
-public abstract class AvoidCamsMovementAnimatronic extends Animatronic {
+public abstract class AvoidCamsAnimatronic extends Animatronic {
     protected final List<String> forbiddenCameras;
 
-    public AvoidCamsMovementAnimatronic(String name, double secInterval, HashMap<Integer, Integer> iaDuringNight,
-                                        int maxIaLevel, String camImgPath, String jumpscareGifPath, int jumpscareRepFrames,
-                                        @Nullable List<String> forbiddenCameras) throws ResourceNotFound {
+    public AvoidCamsAnimatronic(String name, double secInterval, HashMap<Integer, Integer> iaDuringNight,
+                                int maxIaLevel, String camImgPath, String jumpscareGifPath, int jumpscareRepFrames,
+                                @Nullable List<String> forbiddenCameras) throws ResourceNotFound {
         super(name, secInterval, iaDuringNight, maxIaLevel, camImgPath, jumpscareGifPath, jumpscareRepFrames);
         this.forbiddenCameras = Objects.requireNonNullElseGet(forbiddenCameras, () -> new ArrayList<>(0));
     }
