e1bdfac632dc0fb2e0118778f19805f5997229e8
Cristichi <cristichi@hotmail.es>
2024-12-08 17:08:56 +0100

    Added clarifying comments.

--- src/main/java/es/cristichi/fnac/io/Settings.java
+++ src/main/java/es/cristichi/fnac/io/Settings.java
@@ -22,8 +22,16 @@ public class Settings {
 
     public static final String SETTINGS_FILE = "settings.yaml";
 
-    public static Settings fromFile(String file) throws IOException {
-        try (FileInputStream inputStream = new FileInputStream(file)) {
+    /**
+     * You need to call {@link Settings#init()} one anywhere else before this method can work.
+     * Retrieves the Settings saved on the specifiled file the filePath is pointing to. If it does not
+     * exist, it will create a new one with the default values.
+     * @param filePath Path to the file.
+     * @return Either a new {@link Settings}, or one filled with the values saved on the specified file.
+     * @throws IOException If file exists but an error happens trying to read it.
+     */
+    public static Settings fromFile(String filePath) throws IOException {
+        try (FileInputStream inputStream = new FileInputStream(filePath)) {
             Settings settings = new Settings();
             Map<String, Object> config = yaml.load(inputStream);
             settings.setFullscreen((Boolean) config.get("fullscreen"));
@@ -34,17 +42,23 @@ public class Settings {
         } catch (FileNotFoundException notFound){
             return new Settings();
         } catch (Exception exception){
-            throw new IOException("Error trying to read the configuration file \""+file+"\".", exception);
+            throw new IOException("Error trying to read the configuration filePath \""+filePath+"\".", exception);
         }
     }
 
     private boolean fullscreen = true;
     private int fps = 60;
-    private double volume = 1f;
+    private double volume = 1;
 
+    /**
+     * Creates a new {@link Settings} with the default values (fullscreen=true, fps=60, volume=1).
+     */
     public Settings() {
     }
 
+    /**
+     * Creates a new {@link Settings} copying the values of another one.
+     */
     public Settings(Settings copy) {
         this();
         setFullscreen(copy.fullscreen);
@@ -52,32 +66,57 @@ public class Settings {
         setVolume(copy.volume);
     }
 
+    /**
+     * @return Whether or not screen should be fullscreen.
+     */
     public boolean isFullscreen() {
         return fullscreen;
     }
 
+    /**
+     * @return Number of target frames per second. It is used for converting real time to in-game frames.
+     *            Not recommended to have more than the system can handler or less than 5.
+     */
     public int getFps() {
         return fps;
     }
 
+    /**
+     * @return Desired global volume, where 1 = 100% and 0 = 0%.
+     */
     public double getVolume() {
         return volume;
     }
 
+    /**
+     * @param fullscreen Whether or not screen should be fullscreen.
+     */
     public void setFullscreen(boolean fullscreen) {
         this.fullscreen = fullscreen;
     }
 
+    /**
+     * @param fps Number of target frames per second. It is used for converting real time to in-game frames.
+     *            Not recommended to have more than the system can handler or less than 5.
+     */
     public void setFps(int fps) {
         this.fps = fps;
     }
 
+    /**
+     * @param volume Desired global volume, where 1 = 100% and 0 = 0%.
+     */
     public void setVolume(double volume) {
         this.volume = volume;
     }
 
-    public void saveToFile(String file){
-        try (Writer writer = new FileWriter(file)) {
+    /**
+     * You need to call {@link Settings#init()} one anywhere else before this method can work.
+     * Saves the current values of this {@link Settings} object into the file in YAML format.
+     * @param filePath Path to file.
+     */
+    public void saveToFile(String filePath){
+        try (Writer writer = new FileWriter(filePath)) {
             LinkedHashMap<String, Object> config = new LinkedHashMap<>(Map.of(
                     "fullscreen", fullscreen,
                     "fps", fps,
@@ -85,7 +124,7 @@ public class Settings {
             ));
             yaml.dump(config, writer);
         } catch (IOException e) {
-            new ExceptionViewer(new IOException("Settings save file could not be saved.", e));
+            new ExceptionViewer(new IOException("Settings save filePath could not be saved.", e));
         }
     }
 }
