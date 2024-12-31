6ee07da6276eb87a8e9cd132bc7f2003742c757b
Cristichi <cristichi@hotmail.es>
2024-12-31 13:41:53 +0100

    Now registering a Night cannot replace a previous one.
    This is so that Nights registered first would be replaced by the default ones.

--- src/main/java/es/cristichi/fnac/nights/NightRegistry.java
+++ src/main/java/es/cristichi/fnac/nights/NightRegistry.java
@@ -1,6 +1,8 @@
 package es.cristichi.fnac.nights;
 
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,17 +15,27 @@ import java.util.List;
  * the requisite of completed Nights.
  */
 public class NightRegistry {
-    private static final ArrayList<NightFactory> nightRegistry = new ArrayList<>(7);
+    private static final Logger logger = LoggerFactory.getLogger(NightRegistry.class);
+    
+    private static final ArrayList<NightFactory> registry = new ArrayList<>(7);
     
     /**
      * It registers athe {@link NightFactory} for the Night that is available with the given exact
-     * number of completed Nights. If a Night with the same requirement of completed Nights exists, it's replaced.
+     * number of completed Nights. If a Night with the same requirement of completed Nights exists, this does nothing.
      * @param requiredCompletedNights Exact number of completed Nights the player must have in their save file to
      *                                have this Night be the next one to play.
      * @param nightFactory The NightFactory that can create the correct next Night for the player to play.
      */
     public static void registerNight(int requiredCompletedNights, NightFactory nightFactory){
-        nightRegistry.set(requiredCompletedNights, nightFactory);
+        if (requiredCompletedNights < 0){
+            throw new IllegalArgumentException("There cannot be less than 0 completed Nights.");
+        }
+        if (requiredCompletedNights < registry.size()){
+            logger.info("Night for {} completed Nights has been registered.", requiredCompletedNights);
+            registry.add(requiredCompletedNights, nightFactory);
+        } else {
+            logger.info("Night for {} completed Nights was already set.", requiredCompletedNights);
+        }
     }
     
     /**
@@ -31,16 +43,16 @@ public class NightRegistry {
      * @return The factory for this number of completed Nights, or <code>null</code> if the player completed all Nights.
      */
     public static @Nullable NightFactory getNight(int completedNights){
-        if (completedNights < 0 || completedNights>=nightRegistry.size()){
+        if (completedNights < 0 || completedNights>= registry.size()){
             return null;
         }
-        return nightRegistry.get(completedNights);
+        return registry.get(completedNights);
     }
     
     /**
      * @return An ordered, unmodifiable {@link List<NightFactory>} of all Nights registered.
      */
     public static List<NightFactory> getAllNights(){
-        return Collections.unmodifiableList(nightRegistry);
+        return Collections.unmodifiableList(registry);
     }
 }
