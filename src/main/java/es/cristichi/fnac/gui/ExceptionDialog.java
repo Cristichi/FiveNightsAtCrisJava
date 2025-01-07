7425e684d79e0965d708ef9f1df2a201b52a319b
Cristichi <cristichi@hotmail.es>
2025-01-07 11:37:39 +0100

    Tweaked ExceptionDialog for bigger text and better window size.
    Also added some color to it and using the Consolas font for a more old console/terminal look.

--- src/main/java/es/cristichi/fnac/gui/ExceptionDialog.java
+++ src/main/java/es/cristichi/fnac/gui/ExceptionDialog.java
@@ -16,40 +16,84 @@ public class ExceptionDialog extends JFrame {
      *
      * @param exception    Exception that must be informed to the player.
      * @param fatal        Whether closing this window should close the Java application entirely.
-     * @param clearMessage Whether it shows only the message of the error, or otherwise the entire stacktrace.
+     * @param clearMessage Whether it shows only the message of {@code exception}, or otherwise the entire stacktrace.
      * @param logger       Logger to be used to log the error, or {@code null} to not log the error.
      */
     public ExceptionDialog(Exception exception, boolean fatal, boolean clearMessage, Logger logger) {
-        super("Exception Viewer");
-        if (logger != null)
-            logger.error("Error shown to player:", exception);
-
-        if (fatal)
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        super("Opps! An error happened");
+        if (logger != null) logger.error("Error shown to player:", exception);
+        
+        if (fatal) setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        else setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        setMinimumSize(new Dimension(600, 400));
         setLayout(new BorderLayout());
-
+        
         JTextArea textArea = new JTextArea();
+        textArea.setBackground(Color.BLACK);
+        textArea.setForeground(Color.GREEN);
+        textArea.setMargin(new Insets(10,10,10,10));
         textArea.setEditable(false);
-        textArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
-
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(new Font("Consolas Arial", Font.PLAIN, 14));
+        
         StringWriter stringWriter = new StringWriter();
+        if (fatal) {
+            stringWriter.write("""
+                    A very bad error happened, we have to close the game. Please feel free to reopen the game \
+                    and try again what you were doing.
+                    
+                    """);
+        } else {
+            stringWriter.write("""
+                    Oops! An error happened. We can handle it but we wanted you to know. You can read this window \
+                    and close it.
+                    
+                    """);
+        }
         if (clearMessage) {
-            stringWriter.write(exception.getMessage());
+            stringWriter.write("Error message: "+exception.getMessage());
         } else {
             exception.printStackTrace(new PrintWriter(stringWriter));
         }
         textArea.setText(stringWriter.toString());
-
+        
         JScrollPane scrollPane = new JScrollPane(textArea);
         add(scrollPane, BorderLayout.CENTER);
-
-        setSize(600, 400);
-
+        
+        if (fatal) {
+            setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        } else {
+            setVisible(true);
+            Rectangle windowBounds = getBounds();
+            GraphicsDevice[] graphicsDevices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+            GraphicsDevice bestScreen = null;
+            int bestArea = 0;
+            for (GraphicsDevice graphicsDevice : graphicsDevices) {
+                GraphicsConfiguration[] graphicsConfigurations = graphicsDevice.getConfigurations();
+                
+                for (GraphicsConfiguration graphicsConfiguration : graphicsConfigurations) {
+                    Rectangle graphicsBounds = graphicsConfiguration.getBounds();
+                    
+                    Rectangle intersection = windowBounds.intersection(graphicsBounds);
+                    
+                    int intersectionArea = intersection.width * intersection.height;
+                    if (intersectionArea > bestArea) {
+                        bestScreen = graphicsDevice;
+                        bestArea = intersectionArea;
+                    }
+                }
+            }
+            if (bestScreen != null) {
+                int width = bestScreen.getDisplayMode().getWidth();
+                int height = bestScreen.getDisplayMode().getHeight();
+                setSize(width / 2, (int) (height / 1.5));
+            }
+        }
         setLocationRelativeTo(null);
-
-        setAlwaysOnTop(true);
-
         setVisible(true);
+        
+        setAlwaysOnTop(true);
     }
 }
