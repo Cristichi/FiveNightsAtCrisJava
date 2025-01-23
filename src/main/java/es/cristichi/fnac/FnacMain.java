54d0663c233ed1ab23117b7dc521588156b5e64c
Cristichi <cristichi@hotmail.es>
2025-01-23 13:19:09 +0100

    v0.8.13

--- src/main/java/es/cristichi/fnac/FnacMain.java
+++ src/main/java/es/cristichi/fnac/FnacMain.java
@@ -48,7 +48,7 @@ public class FnacMain {
      * When enabled, all Nights are available in the menu.
      */
     @SuppressWarnings("CanBeFinal") //This is to be modified by modders using this as dependency.
-    public static boolean DEBUG_ALLNIGHTS = true;
+    public static boolean DEBUG_ALLNIGHTS = false;
     /**
      * Name of the game.
      */
